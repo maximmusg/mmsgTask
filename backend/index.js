feecdb1347b39a6727565da854e3bb3e34784186
maximmusg <maxil5gomez@gmail.com>
2022-12-28 23:53:34 -0300

    Creando base de datos con MongoDB, conectandola a App y creando variables de entorno para ocultar credenciales

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 
 import  express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
 
 const app = express();
 
-app.listen(4000, () => {
-    console.log('Servidor corriendo en el puerto 4000')
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
