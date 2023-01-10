182cd9810f4d5452a16177b55638c04c25481793
maximmusg <maxil5gomez@gmail.com>
2023-01-09 23:42:53 -0300

    Agregando funcionalidad para resetear passwords

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -77,5 +77,54 @@ const confirmar = async (req, res) => {
     }
 }
 
+const olvidePassword = async (req, res) => {
+    const { email } = req.body;
+    const usuario = await Usuario.findOne({email});
+    if (!usuario) {
+        const error = new Error("El usuario no existe");
+        return res.status(404).json({ msg: error.message });
+    }
 
-export { registrar, autenticar, confirmar };
+    try {
+        usuario.token = generarId()
+        await usuario.save();
+        res.json({msg: "Hemos enviado un email con las instrucciones"});
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+const comprobarToken = async (req, res) => {
+    const { token } = req.params;
+
+    const tokenValido = await Usuario.findOne({ token });
+    if (tokenValido) {
+        res.json({ msg: 'Token valido y el usuario existe' })
+    } else {
+        const error = new Error("Token no valido");
+        return res.status(404).json({ msg: error.message })
+    }
+};
+
+const nuevoPassword = async (req,res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const usuario = await Usuario.findOne({ token });
+
+    if(usuario){
+        usuario.password = password;
+        usuario.token = "";
+        try {
+            await usuario.save();
+            res.json({ msg: "Password modificado Correctamente" })
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        const error = new Error("Token no valido");
+        return res.status(404).json({ msg: error.message });
+    }
+};
+
+export { registrar, autenticar, confirmar, olvidePassword, comprobarToken, nuevoPassword };
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,13 +1,23 @@
 import  express  from "express";
 const router = express.Router();
 
-import {registrar, autenticar, confirmar} from '../controllers/usuarioController.js'
+import {
+    registrar, 
+    autenticar, 
+    confirmar, 
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword
+} from '../controllers/usuarioController.js'
 
 //autenticacion , registro y confirmaciones de usuarios
 
 router.post('/', registrar); //crea un nuevo usuario
 router.post("/login", autenticar);
-router.get("/confirmar/:token", confirmar)
+router.get("/confirmar/:token", confirmar);
+router.post("/olvide-password", olvidePassword);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword)
+
 
 
 export default router;
